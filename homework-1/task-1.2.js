90e7de1f09fedae434979b9c2d5b656da43ee531
Eduard Fedoruk <efedoruk@vertafore.com>
2021-02-16 13:28:02 +0200

    add different versions

--- homework-1/task-1.2.js
+++ homework-1/task-1.2.js
@@ -1,10 +1,18 @@
-import { createReadStream, createWriteStream } from 'fs';
+import { createReadStream, createWriteStream, promises as fs } from 'fs';
 import path from 'path';
 import csv from 'csvtojson';
 
 const csvPath = path.join(__dirname, './csv/annual-enterprise-survey-2019-financial-year-provisional-csv.csv');
 const csvOutputPath = path.join(__dirname, './csv/test.txt');
 
+// stream version
 csv()
   .fromStream(createReadStream(csvPath))
   .pipe(createWriteStream(csvOutputPath));
+
+// RAM load version
+// csv()
+//   .fromFile(csvPath)
+//   .then(JSON.stringify)
+//   .then((csvJSON) => fs.writeFile(csvOutputPath, csvJSON))
+//   .catch(console.error);
